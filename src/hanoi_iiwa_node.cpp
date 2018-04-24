5dba61b53659f1480d0a0426f26b81603c496634
mlab8 <you@example.com>
2018-04-24 10:55:02 +0200

    Change to linear motion via ROS SmartServoLin.

--- src/hanoi_iiwa_node.cpp
+++ src/hanoi_iiwa_node.cpp
@@ -112,6 +112,7 @@ public:
   }
   
   void gripperInit() {
+    ROS_INFO("Initializing gripper...");
     gripper.setInitialization(INIT_ACTIVATION);
     gripper.setGraspingMode(GRASP_PINCH);
     gripper.setActionMode(ACTION_GO);
@@ -169,39 +170,27 @@ public:
     }
   }
 
-  void moveSlice(int from, int to) {
+  void moveSlice(int from, int to, bool approvalRequired = true) {
     assert(from >= 0 && from <= 2);
     assert(to >= 0 && to <= 2);
 
     std::vector<geometry_msgs::Pose> waypoints;
-    geometry_msgs::Pose pose_above_from = tower_poses_[from].pose;
-    pose_above_from.position.z += 0.133;
+    geometry_msgs::PoseStamped pose_above_from = tower_poses_[from];
+    pose_above_from.pose.position.z += 0.133;
     for (int i = 0; i < (13 - tower_nSlices_[from] + 1); ++i) {
-      pose_above_from.position.z -= 0.01;
-      waypoints.push_back(pose_above_from);
+      pose_above_from.pose.position.z -= 0.01;
+      waypoints.push_back(pose_above_from.pose);
     }
-    moveAlongCartesianPathInWorldCoords(waypoints, 0.01, 0, true, false);
+    moveAlongCartesianPathInWorldCoords(waypoints, 0.01, 0, true, approvalRequired);
 
     gripperClose();
     tower_nSlices_[from]--;
 
-    moveit_msgs::OrientationConstraint ocm;
-    ocm.link_name = move_group_.getEndEffectorLink();
-    geometry_msgs::PoseStamped current_pose = getPose();
-    ocm.header.frame_id = current_pose.header.frame_id;
-    ocm.orientation.w = current_pose.pose.orientation.w;
-    ocm.orientation.x = current_pose.pose.orientation.x;
-    ocm.orientation.y = current_pose.pose.orientation.y;
-    ocm.orientation.z = current_pose.pose.orientation.z;
-    ocm.absolute_x_axis_tolerance = 0.01;
-    ocm.absolute_y_axis_tolerance = 0.01;
-    ocm.absolute_z_axis_tolerance = 0.01;
-    ocm.weight = 1.0;
-    moveit_msgs::Constraints constraints;
-    constraints.orientation_constraints.push_back(ocm);
-    move_group_.setPathConstraints(constraints);
+    pose_above_from = tower_poses_[from];
+    pose_above_from.pose.position.z += 0.001;//0.133;
+    publishPoseGoalLinear(pose_above_from);
 
-    std::reverse(waypoints.begin(), waypoints.end());
+    waypoints.clear();
 
     geometry_msgs::Pose pose_to = tower_poses_[to].pose;
     pose_to.position.z += 0.133;
@@ -210,26 +199,25 @@ public:
       pose_to.position.z -= 0.01;
       waypoints.push_back(pose_to);
     }
-    moveAlongCartesianPathInWorldCoords(waypoints, 0.01, 0, true, false);
-    move_group_.clearPathConstraints();
+    moveAlongCartesianPathInWorldCoords(waypoints, 0.01, 0, true, approvalRequired);
 
     gripperOpen();
 
     pose_to.position.z += 0.03;
-    planAndMove(pose_to, false);
+    planAndMove(pose_to, approvalRequired);
 
     tower_nSlices_[to]++;
   }
 
-  void moveTower(int height, int from, int to, int with) {
+  void moveTower(int height, int from, int to, int with, bool approvalRequired = true) {
     assert(from >= 0 && from <= 2);
     assert(to >= 0 && to <= 2);
     assert(with >= 0 && with <= 2);
 
     if (height >= 1) {
-      moveTower(height-1, from, with, to);
-      moveSlice(from, to);
-      moveTower(height-1, with, to, from);
+      moveTower(height-1, from, with, to, approvalRequired);
+      moveSlice(from, to, approvalRequired);
+      moveTower(height-1, with, to, from, approvalRequired);
     }
     else if (height < 0) {
       for (int i = 0; i < 3; ++i) {
@@ -291,8 +279,15 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  std::vector<double> base_pose_jointSpace{0.0784100294113,  0.237325459719, -0.013680412434, -1.38437616825, 0.0258844885975, 1.46759164333, -0.739341557026};
+  //std::vector<double> base_pose_jointSpace{0.0784100294113,  0.237325459719, -0.013680412434, -1.38437616825, 0.0258844885975, 1.46759164333, -0.739341557026};
 
+  // Vertical
+  //std::vector<double> base_pose_jointSpace{-0.457898765802, -0.422531723976, 0.380062669516, -1.8333132267, -2.06858372688, -0.089888997376, -0.190907388926};
+  //std::vector<double> base_pose_jointSpace{-1.61612892151, -0.928053498268, 0.256144344807, -1.77630400658, -1.52423846722, -1.36820602417, -0.101872801781};
+  //std::vector<double> base_pose_jointSpace{1.98734986782, 0.901650428772, 1.82360994816, 1.2087996006, -0.736485004425, -0.849012613297, 1.00593841076};
+  std::vector<double> base_pose_jointSpace{-1.38016748428, -0.778217494488, 2.38215756416, -1.00167918205, -1.14128601551, 1.17012655735, 1.29470527172};
+
+  /*
   geometry_msgs::PoseStamped tow0_pose;
   tow0_pose.header.frame_id = "world";
   tow0_pose.pose.position.x = 0.634;
@@ -308,6 +303,25 @@ int main(int argc, char **argv)
 
   geometry_msgs::PoseStamped tow2_pose = tow1_pose;
   tow2_pose.pose.position.y += 0.32;
+  */
+
+  geometry_msgs::PoseStamped tow0_pose;
+  tow0_pose.header.frame_id = "world";
+  tow0_pose.pose.position.x = 0.634 + 0.072;
+  tow0_pose.pose.position.y = -0.283443 + 0.647;
+  tow0_pose.pose.position.z = 0.985;
+  tow0_pose.pose.orientation.x = 0.963151;
+  tow0_pose.pose.orientation.y = 0.26867;
+  tow0_pose.pose.orientation.z = 0.00855197;
+  tow0_pose.pose.orientation.w = 0.0091728;
+
+  geometry_msgs::PoseStamped tow1_pose = tow0_pose;
+  tow1_pose.pose.position.x -= 0.04;
+  tow1_pose.pose.position.y += 0.28;
+
+  geometry_msgs::PoseStamped tow2_pose = tow1_pose;
+  tow2_pose.pose.position.x -= 0.32;
+  tow2_pose.pose.position.y -= 0.01;
 
   hanoi::HanoiRobot hanoi_robot(&node_handle, "manipulator", base_pose_jointSpace, 3, 0.01);
   hanoi_robot.setTowerPose(0, tow0_pose);
@@ -319,13 +333,14 @@ int main(int argc, char **argv)
   //hanoi_robot.planAndMove(hanoi_robot.getTowerPose(0), true);
   //hanoi_robot.checkPoses();
   //return 0;
-  //hanoi_robot.planAndMoveToBasePose();
+  //while (!hanoi_robot.getMFButtonState()) {};
+  hanoi_robot.planAndMoveToBasePose();
   hanoi_robot.gripperInit();
   hanoi_robot.waitForApproval();
-  hanoi_robot.moveInACoolAndCoodUpwardAndDownwardMotion();
-  return 0;
+  //hanoi_robot.moveInACoolAndCoodUpwardAndDownwardMotion();
+  //return 0;
 
-  hanoi_robot.moveTower(3, 0, 2, 1);
+  hanoi_robot.moveTower(3, 0, 2, 1, true);
   hanoi_robot.planAndMoveToBasePose(false);
   
   ros::shutdown();
